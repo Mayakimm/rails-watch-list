ff9c78e178dd491f0cd95f766ece029c8dfaf613
Maya Kim <mayagy.kim@gmail.com>
2024-07-07 10:31:18 +0200

    trying to make cloudinary to work

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,6 +14,13 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+    if params[:list][:photo].present?
+      # Upload image to Cloudinary
+      result = Cloudinary::Uploader.upload(params[:list][:photo].tempfile.path, options)
+
+      # Store the Cloudinary public ID in your model attribute
+      @list.photo = result['public_id']
+    end
     if @list.save
       redirect_to list_path(@list), notice: "List was successfully created."
     else
@@ -24,6 +31,11 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
+  end
+
+  def options
+    # Add any Cloudinary upload options you need here
+    { crop: :fill }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "lists#index"
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: "lists#index"
+  resources :lists, only: [ :show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
   # root "movies#index"
